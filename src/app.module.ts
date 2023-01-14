b4a0fc8962c3cf9a5ba2344ed8273bc4b503cb37
actuallyzefe <zefek10@gmail.com>
2023-01-15 00:48:31 +0300

    report entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Report } from './reports/report.entity';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
@@ -12,7 +13,7 @@ import { UsersModule } from './users/users.module';
     TypeOrmModule.forRoot({
       type: 'sqlite', // kullandıgımız database
       database: 'db.sqlite',
-      entities: [User], // repo
+      entities: [User, Report], // repo
       synchronize: true,
     }),
     ReportsModule,
